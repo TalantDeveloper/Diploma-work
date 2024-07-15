cddae44e67c2383ae32447b11d12985a82992606
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2024-07-15 11:12:25 +0500

    Don't deploy SECRET_KEY
    Don't deploy SECRET_KEY

--- Diploma_work/settings.py
+++ Diploma_work/settings.py
@@ -1,18 +1,18 @@
 import os
 from pathlib import Path
-
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv(dotenv_path=BASE_DIR / '.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2tfbczt#75mzm*=5#=cpp9-f0y@e(+gzrw_-qror6r(0kmla%h'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -117,8 +117,12 @@ WSGI_APPLICATION = 'Diploma_work.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
--- Diploma_work/urls.py
+++ Diploma_work/urls.py
@@ -7,21 +7,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
+urlpatterns = i18n_patterns(
     path('', include('main.urls')),
+    path('admin/', admin.site.urls),
     path('api/', include('main.api.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 
-]
-
-
-# model-translation urls
-urlpatterns = [
-    *i18n_patterns(*urlpatterns, prefix_default_language=True),
-
-]
-
+)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
